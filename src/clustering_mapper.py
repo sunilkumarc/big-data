48e874caaa42bda2ae053ce50878d8d2aaba450e
Sunil Kumar <sunilc932@gmail.com>
2013-10-26 19:37:55 +0530

    modified clustering_mapper.py

--- src/clustering_mapper.py
+++ src/clustering_mapper.py
@@ -7,8 +7,6 @@ def emit():
 	    line = line.strip()
 	    columns = line.split("\t")
 	    
-	    # for column in columns:
-	    #     print '%s\t%s' % (column, 1)
 	    print "%s\t%s\t%s\t%s" % (columns[0], columns[1], columns[2], columns[3])
 
 def mapper():
